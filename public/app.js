961d0944c9c29b794d401ceb4cb4c50480441e4a
Moddy Pikiso <moddypikiso17@gmail.com>
2021-11-25 16:54:00 +0200

    first commit

---
+++ public/app.js
@@ -0,0 +1,43 @@
+const color = document.querySelector('.colors');
+const brand = document.querySelector('.brands');
+const car = document.querySelector('.cars');
+
+
+
+const templateColor = document.querySelector('.colorT');
+const template1 = Handlebars.compile(templateColor.innerText);
+
+const templateMake = document.querySelector('.makeT');
+const template2 = Handlebars.compile(templateMake.innerText);
+
+const templateCar = document.querySelector('.carT');
+const template3 = Handlebars.compile(templateCar.innerText);
+
+axios.get('http://api-tutor.herokuapp.com/v1/colors')
+
+    .then(function (result) {
+
+        color.innerHTML = template1({ color: result.data });
+
+    });
+
+axios.get('http://api-tutor.herokuapp.com/v1/makes')
+
+    .then(function (result) {
+
+        brand.innerHTML = template2({ make: result.data });
+
+    })
+
+axios.get('http://api-tutor.herokuapp.com/v1/cars')
+
+    .then(function (result) {
+
+        car.innerHTML = template3({ car: result.data })
+
+    })
+
+
+
+
+
---
+++ public/filter.js
@@ -0,0 +1,47 @@
+const carBrand = document.querySelector('.cB');
+const brandBtnElem = document.querySelector('.btn');
+
+const templateCarB = document.querySelector('.carBrand');
+const template4 = Handlebars.compile(templateCarB.innerText);
+
+
+// const carColors = document.querySelector('.cC');
+// const colorBtnElem = document.querySelector('.btns');
+
+// const templateColor = document.querySelector('.carColor');
+// const template5 = Handlebars.compile(templateColor.innerText);
+
+// specific make for a car
+brandBtnElem.addEventListener("click", function () {
+    var model = document.querySelector('.carBrands').value;
+    var modelColor = document.querySelector('.colorB').value;
+
+    console.log(model)
+
+    axios.get(`http://api-tutor.herokuapp.com/v1/cars/make/${model}/color/${modelColor}`)
+    // /v1/cars/make/:make/color/:car_color	
+        .then(function (result) {
+            console.log(result.data);
+
+            carBrand.innerHTML = template4({ carBrnd: result.data })
+
+        })
+
+
+});
+
+// colorBtnElem.addEventListener("click", function () {
+//     var modelColor = document.querySelector('.colorB').value;
+//     console.log(modelColor)
+
+//     axios.get(`http://api-tutor.herokuapp.com/v1/cars/color/${modelColor}`)
+
+//         .then(function (result) {
+//             console.log(result.data);
+
+//             carColors.innerHTML = template5({ carCol: result.data })
+
+//         })
+
+
+// });
